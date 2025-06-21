4310f5612efb95061c013f486159693e23ce5e7c
ben-from-earth <>
2025-06-21 12:07:22 -0400

    Updated ThreeCard.js with full suedocode logic

--- ThreeCard.js
+++ ThreeCard.js
@@ -6,4 +6,23 @@ class Card{
 
 // Create a deck of cards, maybe a deck function or something like that
 
-//function deal(){} shuffle the deck, initialize the game state 
+//function deal(players){ shuffle the deck, initialize the game state 
+    //randomize deck
+    //deal three cards face down to each player
+    //deal 6 cards face up to each player
+    //place one card from the top of the deck face up in the middle
+// } 
+
+//function play(){
+    //choose three cards to place on top of three face down cards
+    //In order place a card from your deck that is higher than the card on the table
+    //Two is acceptable at anytime and reset the deck
+    //if all cards in hand lower than table card, draw all cards under and including table card and add to hand
+    //if 10 played or four cards in a row same rank send to discard pile 
+    //if hand empty, draw left or right card from face up three on table
+    //if hand empty again on next turn use next card in order until face ups are exhausted
+    //if faceups exhausted use one from either side of facedown and logic plays out the same
+    //declare winner if all cards exhausted 
+// }
+
+//
